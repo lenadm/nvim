319042aedf36f12fa35d6bf2c6d708123ab75577
ben richards <lenarichards2234@gmail.com>
2024-03-03 13:52:36 +0000

    added nvim-dap-ui for debugging

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -64,6 +64,11 @@ require('lazy').setup({
   },
 
   {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap'},
+  },
+
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
