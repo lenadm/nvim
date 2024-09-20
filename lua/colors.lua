1f269a871dda0498635118aa90cf2361df572c99
lena <lenarichards2234@gmail.com>
2024-09-20 22:58:10 +0100

    init

---
+++ lua/colors.lua
@@ -0,0 +1,4 @@
+-- Sets colors to line numbers Above, Current and Below  in this order
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#50C878', bold=true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#D2042D', bold=true })
---
+++ lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- [[ Basic Keymaps ]]
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<c-o", "<c-o>zz")
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- vim: ts=2 sts=2 sw=2 et
