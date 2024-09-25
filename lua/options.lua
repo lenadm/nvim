f855f85d69f611c67976781f8c04297711b05839
lena <lenarichards2234@gmail.com>
2024-09-25 01:18:38 +0100

    switched from tabs to spaces seeing if i want to switch back

--- lua/options.lua
+++ lua/options.lua
@@ -20,7 +20,7 @@ vim.o.mouse = 'a'
 vim.o.clipboard = 'unnamedplus'
 
 -- Indenting setting
-vim.o.expandtab = false
+vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smarttab = false
