c9ffc09620fbf827ffae888933cc63b42b9863d7
lena <lenarichards2234@gmail.com>
2024-11-08 19:50:20 +0000

    added additional keybinding for neogit

--- lua/neogit_config.lua
+++ lua/neogit_config.lua
@@ -4,10 +4,14 @@ vim.keymap.set('n', '<leader>gg', function() neogit.open() end)
 vim.keymap.set('n', '<leader>gc', function() neogit.open({ "commit" }) end)
 vim.keymap.set('n', '<leader>grs', function() neogit.open({ "reset" }) end)
 vim.keymap.set('n', '<leader>grv', function() neogit.open({ "revert" }) end)
+vim.keymap.set('n', '<leader>gph', function() neogit.open({ "push" }) end)
+vim.keymap.set('n', '<leader>gpl', function() neogit.open({ "pull" }) end)
 
 require('which-key').add({
-    { "<leadergg>", desc = "Open Neogit" },
-    { "<leadergc", desc = "Open Neo[g]it [c]ommit" },
-    { "<leadergrs", desc = "Open Neo[g]it [r]e[s]et" },
-    { "<leadergrv", desc = "Open Neo[g]it [r]e[v]ert" },
+    { "<leader>gg", desc = "Open Neogit" },
+    { "<leader>gc", desc = "Open Neo[g]it [c]ommit" },
+    { "<leader>grs", desc = "Open Neo[g]it [r]e[s]et" },
+    { "<leader>grv", desc = "Open Neo[g]it [r]e[v]ert" },
+    { "<leader>gph", desc = "Open Neo[g]it [p]us[h]" },
+    { "<leader>gpl", desc = "Open Neo[g]it [p]ul[l]" },
 })
