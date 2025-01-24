2c992b258c6b014e86bddcfab4558a5429064fa4
lena <lenarichards2234@gmail.com>
2025-01-24 14:19:04 +0000

    Edited some grapple binds

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -14,16 +14,16 @@ require('lazy').setup({
       status = false,
     },
     keys = {
-      { "<leader>ab", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
-      { "<leader>vb", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+      { "<leader>at", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+      { "<leader>lt", "<cmd>Grapple toggle_tags<cr>", desc = "List tags" },
 
       { "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
       { "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
       { "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
       { "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
 
-      { "<leader>k", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
-      { "<leader>j", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
+      { "<leader>Down", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
+      { "<leader>Up", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
     },
   },
 
